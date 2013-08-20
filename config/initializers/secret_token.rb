06772d15f6079d79008e6fad05cbf9cd62b4d94a
Susan Holcomb <thefurtheroff@gmail.com>
2013-08-19 19:51:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BatmanApp::Application.config.secret_token = 'dfee3d7ea567c3b3ce20f10ba073bc912080c48d4154c2cc192b43ef6cf131470a2d4916c3440f35639e7e20341e26456e81318d61dc91df30f6613e5a77b364'
